00b99e46710babd32f4a3313d5bbfc6abeb84903
Yosua Kristianto <yosuakristianto754@gmail.com>
2020-11-11 00:20:11 +0700

    Initial Commit

---
+++ cukx-validate/cukx-validate.js
@@ -0,0 +1,139 @@
+// Only number input allowed 
+var inputNumberOnly = document.getElementsByClassName("input-number-only");
+
+for(let i = 0; i < inputNumberOnly.length; i++){
+    inputNumberOnly[i].addEventListener("keypress", function(evt){
+        if(evt.which < 48 || evt.which > 57)
+            evt.preventDefault();
+    });
+}
+
+// Only alphabeth upper and lower case and space are allowed
+var inputAlphaOnly = document.getElementsByClassName("input-alphabeth-only");
+
+for(let i = 0; i < inputAlphaOnly.length; i++){
+    inputAlphaOnly[i].addEventListener("keypress", function(evt){
+        if((evt.which < 65 || evt.which > 90) && (evt.which < 97 || evt.which > 122) && evt.which != 32)
+            evt.preventDefault();
+    });
+}
+
+// Only Alphanumeric upper and lower case and space are allowed
+var inputAlnumOnly = document.getElementsByClassName("input-alnum-only");
+
+for(let i = 0; i < inputAlnumOnly.length; i++){
+    inputAlnumOnly[i].addEventListener("keypress", function(evt){
+        if((evt.which < 48 || evt.which > 57) && (evt.which < 65 || evt.which > 90) && (evt.which < 97 || evt.which > 122) && evt.which != 32)
+            evt.preventDefault();
+    });
+}
+
+// Only lower case alphabeth are allowed
+var inputLowerOnly = document.getElementsByClassName("input-lower-only");
+
+for(let i = 0; i < inputLowerOnly.length; i++){
+    inputLowerOnly[i].addEventListener("keypress", function(evt){
+        if(evt.which < 97 || evt.which > 122)
+            evt.preventDefault();
+    });
+}
+
+// Only upper case alphabeth are allowed
+var inputUpperOnly = document.getElementsByClassName("input-upper-only");
+
+for(let i = 0; i < inputUpperOnly.length; i++){
+    inputUpperOnly[i].addEventListener("keypress", function(evt){
+        if(evt.which < 65 || evt.which > 90)
+            evt.preventDefault();
+    });
+}
+
+// The form can't pre space
+var cantPreSpace = document.getElementsByClassName("cant-pre-space");
+
+for(let i = 0; i < cantPreSpace.length; i++){
+    cantPreSpace[i].addEventListener("keypress", function(evt){
+        if(this.value == " "){
+            this.value = "";
+            return;
+        }
+
+        if(this.value.charAt(0) == " "){
+            idx = 0;
+            x = this.value;
+
+            for(j = 0; j< this.value.length; j++){
+                if(this.value.charAt(j) != " "){
+                    idx = idx + 1; break;
+                }
+            }
+
+            this.value = this.value.substr(idx, this.value.length);
+        }
+    });
+}
+
+// The form can't space
+var cantSpace = document.getElementsByClassName("cant-space");
+
+for(let i = 0; i < cantSpace.length; i++){
+    cantSpace[i].addEventListener("keypress", function(evt){
+        if(evt.which == 32)
+            evt.preventDefault();
+
+        this.value = this.value.replace(" ", "");
+    });
+}
+
+// The form has clock format => 00 01 02 10 20 59
+var inputClock = document.getElementsByClassName("input-clock");
+
+for(let i = 0; i < inputClock.length; i++){
+    inputClock[i].addEventListener("focus", function(e){
+        inputHour[i].value = 0;
+        this.select();
+    });
+
+    inputClock[i].addEventListener("keypress", function(e){
+        if(inputClock[i].value == 0 && e.which == 48) e.preventDefault();
+    });
+
+    inputClock[i].addEventListener("change", function(){
+        newString = "";
+
+        if(inputClock[i].length != 1 && inputClock[i].value != "0"){
+            pivot = false;
+            for(let j = 0; j< inputClock[i].value.length; j++){
+                if(inputClock[i].value.charAt(j) != "0") pivot = true;
+
+                if(pivot) newString += inputClock[i].value.charAt(j);
+            }
+        }
+
+        inputClock[i].value = newString;
+
+        if(inputClock[i].value < 10){
+            inputClock[i].value = "0" + inputClock[i].value;
+        }
+    });
+}
+
+// The form is hour input
+var inputHour = document.getElementsByClassName("input-hour");
+
+for(let i = 0; i < inputHour.length; i++){
+    inputHour[i].addEventListener("keypress", function(evt){
+        if(inputHour[i].value > 23) inputHour[i].value = 23;
+        if(inputHour[i].value < 0) inputHour[i].value = 0;
+    });
+}
+
+// The form is minute input
+var inputMinute = document.getElementsByClassName("input-minute");
+
+for(let i = 0; i < inputMinute.length; i++){
+    inputMinute[i].addEventListener("keypress", function(evt){
+        if(inputMinute[i].value > 59) inputMinute[i].value = 59;
+        if(inputMinute[i].value < 0) inputMinute[i].value = 0;
+    });
+}
---
+++ readme.md
@@ -0,0 +1,43 @@
+## Cux Js
+
+Javascript utilities for you who're just too lazy to make validations, and utilities.
+
+Just another Javascript helper library for free.
+
+> Advantages
+> * No other Javascript libraries are required
+> * Development version for you to learn or even contribute
+> * Minified version for you to use
+
+## What's inside this repository?
+
+### 1. cukx-validate.js
+#### Contains form standard validations like only number allowed, only uppercase allowed, no space allowed, etc.
+
+## How to use?
+
+1. Include `cukx-validate.js` to your view file
+
+> `<script type="text/javascript" src="cukx-validate.js"></script>`
+
+2. Add class for desired validations
+
+> `<input type="text" class="input-number-only" />`
+
+### Also support `<textarea>`
+
+> `<textarea class="input-alnum-only" maxlength="100"></textarea>`
+
+## Supported validations
+
+| NO | Class Name | Function |
+|---|---|---|
+| 1 | `input-number-only` | The text field only support number input. (Can't space) |
+| 2 | `input-alphabeth-only` | The text field support alphabeth and space input only. |
+| 3 | `input-alnum-only` | The text field support alphanumeric and space input only. |
+| 4 | `input-lower-only` | The text field support lowercase alphabeth only. |
+| 5 | `input-upper-only` | The text field support uppercase alphabeth only. |
+| 6 | `cant-pre-space` | The text field can't have pre space as value. |
+| 7 | `cant-space` | The text field can't have space. |
+| 8 | `input-clock input-hour` | The text field have hour clock rule. (00-23) |
+| 9 | `input-clock input-minute` | The text field have minute clock rule. (00-59) |
